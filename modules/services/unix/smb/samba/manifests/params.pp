66145162b6b8fe25b16367bf398665df80bbc216
alixxhiscock <hiscockalix@gmail.com>
2025-11-21 18:55:35 +0000

    renamed previous samba to sambaOld and new version to samba

--- modules/services/unix/smb/samba/manifests/params.pp
+++ modules/services/unix/smb/samba/manifests/params.pp
@@ -1,118 +1,92 @@
-# Class: samba::params
-#
-# This class defines default parameters used by the main module class samba
-# Operating Systems differences in names and paths are addressed here
-#
-# == Variables
-#
-# Refer to samba class for the variables defined here.
-#
-# == Usage
-#
-# This class is not intended to be used directly.
-# It may be imported or inherited by other classes
-#
-class samba::params {
+# default parameters
 
-  ### Application related parameters
-
-  $package = $::operatingsystem ? {
-    default => 'samba',
+class samba::params(
+  Boolean $sernetpkgs = false,
+){
+  if $sernetpkgs {
+    fail('sernetpkgs is not supported anymore as these packages are EOL')
+  }else{
+    case $facts['os']['family'] {
+      'redhat': {
+          $cleanup                = undef
+          $packagesambadc         = 'samba-dc'
+          $packagesambaclassic    = 'samba'
+          $packagesambawinbind    = 'samba-winbind'
+          $packagesambansswinbind = 'samba-winbind-clients'
+          $packagesambapamwinbind = 'samba-winbind-clients'
+          $packagesambaclient     = 'samba-client'
+          # for now, this is not supported by Debian
+          $servivesambadc         = undef
+          $servivesmb             = 'smb'
+          $servivewinbind         = 'winbind'
+          $sambacmd               = '/usr/bin/samba-tool'
+          $sambaclientcmd         = '/usr/bin/smbclient'
+          $sambaoptsfile          = '/etc/sysconfig/samba'
+          $sambaoptstmpl          = "${module_name}/redhat-samba.erb"
+          $smbconffile            = '/etc/samba/smb.conf'
+          $krbconffile            = '/etc/krb5.conf'
+          $packagepyyaml          = 'PyYAML'
+      }
+      'Debian': {
+          $cleanup                = 'pkill -9 smbd; pkill -9 nmbd; pkill -9 samba; rm -rf /var/run/samba; /bin/true'
+          $packagesambadc         = 'samba'
+          $packagesambaclassic    = 'samba'
+          $packagesambawinbind    = 'winbind'
+          $packagesambansswinbind = 'libnss-winbind'
+          $packagesambapamwinbind = 'libpam-winbind'
+          $packagesambaclient     = 'smbclient'
+          $servivesambadc         = 'samba-ad-dc'
+          if $facts['os']['name'] == 'Ubuntu' {
+            $servivesmb           = 'smbd'
+          } elsif ($facts['os']['name'] == 'Debian') and ($facts['os']['release']['major'] >= '8') {
+            $servivesmb           = 'smbd'
+          } else {
+            $servivesmb           = 'samba'
+          }
+          $servivewinbind         = 'winbind'
+          $sambacmd               = '/usr/bin/samba-tool'
+          $sambaclientcmd         = '/usr/bin/smbclient'
+          $sambaoptsfile          = '/etc/default/samba4'
+          $sambaoptstmpl          = "${module_name}/debian-samba.erb"
+          $smbconffile            = '/etc/samba/smb.conf'
+          $krbconffile            = '/etc/krb5.conf'
+          $packagepyyaml          = 'python-yaml'
+      }
+      'Archlinux': {
+          $cleanup                = undef
+          $packagesambadc         = 'samba'
+          $packagesambaclassic    = 'samba'
+          $packagesambawinbind    = 'libwbclient'
+          $packagesambansswinbind = 'libnss-winbind'
+          $packagesambapamwinbind = 'libpam-winbind'
+          $packagesambaclient     = 'smbclient'
+          $servivesambadc         = 'samba'
+          $servivesmb             = 'smbd'
+          $servivewinbind         = 'winbindd'
+          $sambacmd               = '/usr/bin/samba-tool'
+          $sambaclientcmd         = '/usr/bin/smbclient'
+          $sambaoptsfile          = '/etc/default/samba4'
+          $sambaoptstmpl          = "${module_name}/debian-samba.erb"
+          $smbconffile            = '/etc/samba/smb.conf'
+          $krbconffile            = '/etc/krb5.conf'
+          $packagepyyaml          = 'python2-yaml'
+      }
+      default: {
+          fail('unsupported os')
+      }
+    }
   }
 
-  $service = $::operatingsystem ? {
-    /(?i:Centos|RedHat|Scientific|Fedora|Amazon|Linux)/ => 'smb',
-    /(?i:SLES|OpenSuSE)/                                => 'smb',
-    /(?i:Ubuntu|Mint)/                                  => 'smbd',
-    /(?i:Debian)/                                       => 'samba',
-    default                                             => 'smb',
-  }
+  $sambaaddtool     = '/usr/local/bin/additional-samba-tool'
+  $nsswitchconffile = '/etc/nsswitch.conf'
+  $sambacreatehome  = '/usr/local/bin/smb-create-home.sh'
 
-  $service_status = $::operatingsystem ? {
-    default => true,
-  }
-
-  $process = $::operatingsystem ? {
-    default => 'smbd',
-  }
-
-  $process_args = $::operatingsystem ? {
-    default => '',
-  }
-
-  $process_user = $::operatingsystem ? {
-    default => 'root',
-  }
-
-  $config_dir = $::operatingsystem ? {
-    default => '/etc/samba',
-  }
-
-  $config_file = $::operatingsystem ? {
-    default => '/etc/samba/smb.conf',
-  }
-
-  $config_file_mode = $::operatingsystem ? {
-    default => '0644',
-  }
-
-  $config_file_owner = $::operatingsystem ? {
-    default => 'root',
-  }
-
-  $config_file_group = $::operatingsystem ? {
-    default => 'root',
-  }
-
-  $config_file_init = $::operatingsystem ? {
-    /(?i:Debian|Ubuntu|Mint)/ => '/etc/default/samba',
-    default                   => '/etc/sysconfig/samba',
-  }
-
-  $pid_file = $::operatingsystem ? {
-    /(?i:Debian|Ubuntu|Mint)/ => '/var/run/samba/smbd.pid',
-    default                   => '/var/run/smbd.pid',
-  }
-
-  $data_dir = $::operatingsystem ? {
-    default => '/etc/samba',
-  }
-
-  $log_dir = $::operatingsystem ? {
-    default => '/var/log/samba',
-  }
-
-  $log_file = $::operatingsystem ? {
-    default => [ '/var/log/samba/log.smbd','/var/log/samba/log.nmbd' ],
-  }
-
-  $port = '445'
-  $protocol = 'tcp'
-
-  # General Settings
-  $my_class = ''
-  $source = ''
-  $source_dir = ''
-  $source_dir_purge = false
-  $template = ''
-  $options = ''
-  $service_autorestart = true
-  $version = 'present'
-  $absent = false
-  $disable = false
-  $disableboot = false
-
-  ### General module variables that can have a site or per module default
-  $monitor = false
-  $monitor_tool = ''
-  $monitor_target = $::ipaddress
-  $firewall = false
-  $firewall_tool = ''
-  $firewall_src = '0.0.0.0/0'
-  $firewall_dst = $::ipaddress
-  $puppi = false
-  $puppi_helper = 'standard'
-  $debug = false
-  $audit_only = false
-
+  $logclasslist =  [
+    'all',     'tdb',     'printdrivers', 'lanman',   'smb',
+    'rpc_srv', 'rpc_cli', 'passdb',       'sam',      'auth',
+    'winbind', 'vfs',     'idmap',        'quota',    'acls',
+    'locking', 'msdfs',   'dmapi',        'registry', 'rpc_parse',
+    ]
 }
+
+# vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
