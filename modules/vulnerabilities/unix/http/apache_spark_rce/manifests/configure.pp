657352a662c53994818a74adf2863cddc3afe9a5
alixthegreat <hiscockalix@gmail.com>
2025-12-18 14:10:45 +0000

    fixed preleak, changed to home directory

--- modules/vulnerabilities/unix/http/apache_spark_rce/manifests/configure.pp
+++ modules/vulnerabilities/unix/http/apache_spark_rce/manifests/configure.pp
@@ -7,6 +7,8 @@ class apache_spark_rce::configure {
   $leaked_filenames = $secgen_parameters['leaked_filenames']
   $strings_to_leak = $secgen_parameters['strings_to_leak']
   $user = $secgen_parameters['unix_username'][0]
+  $pre_leaked_filenames = $secgen_parameters['pre_leaked_filenames']
+  $strings_to_pre_leak = $secgen_parameters['strings_to_pre_leak']
 
   Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ] }
 
@@ -23,7 +25,18 @@ class apache_spark_rce::configure {
     storage_directory => "/home/${user}",
     leaked_filenames  => $leaked_filenames,
     strings_to_leak   => $strings_to_leak,
-    owner             => 'root',
+    owner             => 'spark',
+    mode              => '0750',
+    leaked_from       => 'apache_spark_rce',
+  }
+
+  # Not really preleaking, hard to change spark config to pre leak.
+  # TODO in future: Actually preleak it through http-title to show in nmap scan
+  ::secgen_functions::leak_files { 'spark-flag-pre-leak':
+    storage_directory => "/home/${user}",
+    leaked_filenames  => $pre_leaked_filenames,
+    strings_to_leak   => $strings_to_pre_leak,
+    owner             => 'spark',
     mode              => '0750',
     leaked_from       => 'apache_spark_rce',
   }
